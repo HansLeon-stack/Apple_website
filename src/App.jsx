9591ccb3c0d5ba45c343f2d29579c7c3e5aecb50
Hans Leonard <hans.leonardd@gmail.com>
2025-12-11 20:52:02 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./components/navbar"
+import Highlights from "./components/Highlights"
+import Hero from "./components/Hero"
+import Model from "./components/Model"
+import Features from "./components/Features"
+import Works from "./components/Works"
+import Footer from "./components/Footer"
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <Works />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
